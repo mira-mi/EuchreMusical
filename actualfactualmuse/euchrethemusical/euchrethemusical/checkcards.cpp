58dba673a63c988621302baf599532e026d1039f
Maya K Hunter <67883744+mira-mi@users.noreply.github.com>
2023-04-17 22:43:55 -0500

    more user input

---
+++ actualfactualmuse/euchrethemusical/euchrethemusical/checkcards.cpp
@@ -0,0 +1,115 @@
+#ifndef checkcards
+#define CHECKCARDS  
+#include "checkcards.h"
+#include "deck.h"
+
+
+
+
+cardChecker::cardChecker(card trump, std::vector<card>& table) : trump(trump), bench(table) {
+    //scared to remove this
+}
+
+void cardChecker::setars(card trump, std::vector<card>& table) {
+    this->trump = trump;
+    this->bench = table;
+};
+
+card cardChecker::winningCard() {
+
+    card winner = bench[0];
+    std::string leadingSuit = bench[0].Suit;
+
+    
+    for (int i = 0; i < bench.size(); i++) {
+        if (bench[i].Suit == trump.Suit && bench[i].Suit == "jack") {
+            return bench[i];
+        }
+    }
+    //bower a
+    //i have no idea what I was writing here but I guess I was going to say something about what a bower was.
+    //high trump cards essentially, so yeah
+    std::string bower = gBower();
+    for (int i = 0; i < bench.size(); i++) {
+        if (bench[i].Suit == bower && bench[i].Suit == "jack") {
+            return bench[i];
+        }
+    }
+
+    //check if there are any trump suits
+    if (hasTrump()) {
+        //if has, check highest card of trump suits
+        for (int i = 0; i < bench.size(); i++) {
+            if (bench[i].Suit == trump.Suit) {
+                if (winner.Suit == trump.Suit) {
+                    if (bench[i].Value < winner.Value) {
+                        winner = bench[i];
+                    }
+                }
+                else {
+                    winner = bench[i];
+                }
+            }
+        }
+    }
+    else {
+        //check highest of leading suit
+        for (int i = 0; i < bench.size(); i++) {
+            if (bench[i].Suit == leadingSuit && bench[i].Value < winner.Value) {
+                winner = bench[i];
+            }
+        }
+    }
+    return winner;
+};
+
+bool cardChecker::hasTrump() {
+
+    for (int i = 0; i < bench.size(); i++) {
+        if (bench[i].Suit == trump.Suit) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string cardChecker::gBower() {
+    if (trump.Suit == "hearts") {
+        return "diamonds";
+    }
+    if (trump.Suit == "diamonds") {
+        return "hearts";
+    }
+    if (trump.Suit == "spades") {
+        return "clubs";
+    }
+    if (trump.Suit == "clubs") {
+        return "spades";
+    }
+    return "none";
+}
+
+std::string cardChecker::printBench(std::vector<card> b)
+{
+    return std::string();
+}
+
+std::string cardChecker::printbench(std::vector<card> b) {
+
+    std::string Bench = "\n";
+    Bench += "Bench\n";
+    for (int i = 0; i < b.size(); i++) {
+        Bench += b[i].ShowCard();
+        Bench += "\n";
+    }
+    return Bench;
+
+    // before you question my choices on benches instead of using a flatter surface like a table, at my high school
+    //they used to play cards at the lunch tables so sometimes it would be on the actual bench or the table and other
+    //times it would be on the floor and I used to play cards on my bed
+}
+
+
+
+#endif
+
